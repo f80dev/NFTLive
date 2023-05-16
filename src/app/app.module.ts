6c6b61562485d286b911b92ff52d2800adbcd699
F80 <43803343+f80dev@users.noreply.github.com>
2023-05-16 09:18:35 +0200

    NFTLive first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { FaqsComponent } from './faqs/faqs.component';
+import { AuthentComponent } from './authent/authent.component';
+import { AdminComponent } from './admin/admin.component';
+import { PromptComponent } from './prompt/prompt.component';
+import { PaymentComponent } from './payment/payment.component';
+import { SignatureComponent } from './signature/signature.component';
+import { HourglassComponent } from './hourglass/hourglass.component';
+import { UploadFileComponent } from './upload-file/upload-file.component';
+import { SplashComponent } from './splash/splash.component';
+import { ScannerComponent } from './scanner/scanner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    FaqsComponent,
+    AuthentComponent,
+    AdminComponent,
+    PromptComponent,
+    PaymentComponent,
+    SignatureComponent,
+    HourglassComponent,
+    UploadFileComponent,
+    SplashComponent,
+    ScannerComponent
   ],
   imports: [
     BrowserModule
